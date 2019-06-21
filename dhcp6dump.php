b163730d35a9b5632dd75644041ae9d258beb402
Miloslav Hůla <miloslav.hula@fsv.cvut.cz>
2019-06-21 16:09:08 +0200

    Released version 6

--- dhcp6dump.php
+++ dhcp6dump.php
@@ -13,7 +13,7 @@ require __DIR__ . '/src/DHCPv6VendorOptions.php';
 require __DIR__ . '/src/IANAEnterpriseNumbers.php';
 
 
-define('DHCP6DUMPER_VERSION', '5');
+define('DHCP6DUMPER_VERSION', '6');
 
 
 function usage() {
