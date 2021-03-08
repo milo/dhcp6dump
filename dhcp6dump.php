277a10fdb2c8a64bf3484dc4b3a22e9c2af412b3
Miloslav Hůla <miloslav.hula@gmail.com>
2021-03-08 15:38:48 +0100

    Released version 8

--- dhcp6dump.php
+++ dhcp6dump.php
@@ -13,7 +13,7 @@ require __DIR__ . '/src/DHCPv6VendorOptions.php';
 require __DIR__ . '/src/IANAEnterpriseNumbers.php';
 
 
-define('DHCP6DUMPER_VERSION', '7');
+define('DHCP6DUMPER_VERSION', '8');
 
 
 function usage() {
